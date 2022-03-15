88baf155bafd50d0a351c4fedeac15b2153350f7
HUONG <lenhatuyenhuong@gmail.com>
2022-03-15 14:46:03 +0700

    TH ngày 07/03

---
+++ app/src/main/java/com/example/project_3a_3c/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.project_3a_3c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Mh_3a);
+    }
+}
